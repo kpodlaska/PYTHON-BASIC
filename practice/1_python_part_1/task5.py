58b960a9b597e9119e7835a8cae841a0b88943b8
kpodlaska <kpodlaska@griddynamics.com>
2022-04-20 10:48:39 +0200

    uzupelniony

--- practice/1_python_part_1/task5.py
+++ practice/1_python_part_1/task5.py
@@ -13,4 +13,22 @@ Examples:
 
 
 def remove_duplicated_words(line: str) -> str:
-    ...
+    wynik = []
+    pomocnicza = line.split(" ")
+    print("lista wejsciowa :", pomocnicza)
+    for x in pomocnicza:
+        if x not in wynik:
+            wynik.append(x)
+    print("wynik w postaci listy:", wynik)
+    wynik = [' '.join([str(i) for i in wynik])]
+    print("wynik w postaci jednoelemtowej listy", wynik)
+    print("wynik jako string", wynik[0])
+    print("*********")  # dla czytelnosci
+
+
+remove_duplicated_words('cat dog 3')
+remove_duplicated_words('cat cat dog 1 dog 2')
+remove_duplicated_words('1 2 3')
+remove_duplicated_words('1 2 3 4 5 12 13 2 2 2 3 silly word')
+remove_duplicated_words('1 1 1 1 1 1 1 1 1 1 1 1 2 1 1 1 1 1 1 1 1')
+remove_duplicated_words('cat dog cat cat cat monkey dog dog dog dog dog dog cat cat cat cat cat cat')
