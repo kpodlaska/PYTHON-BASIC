ce9a46f87ff180d9dec1cd71b80079330362d06c
kpodlaska <kpodlaska@griddynamics.com>
2022-04-20 10:50:47 +0200

    uzupelniony

--- practice/1_python_part_1/task6.py
+++ practice/1_python_part_1/task6.py
@@ -9,13 +9,46 @@ Examples:
         34
     >>> get_min_max('filename')
     (-2, 34)
-
 Hint:
 To read file line-by-line you can use this:
 with open(filename) as opened_file:
     for line in opened_file:
         ...
 """
+plik=open("dużo_liczb.txt","w")
+from typing import Tuple
+
+
+def get_min_max(filename: str) -> Tuple[int, int]:
+
+    file=open(filename,"r")
+    lines=file.readlines()
+    file.close()
+
+    for i in lines:
+        print(f'lines:{i}')
+    print(lines)
+    pomocnicza_tablica=[]
+    for i in lines:
+        tymczasowe=i.strip()
+        print(tymczasowe)
+        pomocnicza_tablica.append(tymczasowe)
+
+
+    print(pomocnicza_tablica)
+
+    print("a teraz zapis w formacie jaki chce zadanie:")
+    wynik=[]
+    min_arg=min(pomocnicza_tablica)
+    wynik.append(min_arg)
+    max_arg=max(pomocnicza_tablica)
+    wynik.append(max_arg)
+    wynik2=(min(pomocnicza_tablica),max(pomocnicza_tablica))
+    #print(wynik) #wynik uzyskany dla porównania
+    print(wynik2)
+get_min_max("tekst1.txt")
+get_min_max("tekst2.txt")
+get_min_max("same_parzyste.txt")
 from typing import Tuple
 
 
