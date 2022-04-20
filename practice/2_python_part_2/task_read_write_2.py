c38efec1aa442fc46e7c388b5d848324433aff13
kpodlaska <kpodlaska@griddynamics.com>
2022-04-20 11:48:17 +0200

    uzupelniony

--- practice/2_python_part_2/task_read_write_2.py
+++ practice/2_python_part_2/task_read_write_2.py
@@ -2,10 +2,8 @@
 Use function 'generate_words' to generate random words.
 Write them to a new file encoded in UTF-8. Separator - '\n'.
 Write second file encoded in CP1252, reverse words order. Separator - ','.
-
 Example:
     Input: ['abc', 'def', 'xyz']
-
     Output:
         file1.txt (content: "abc\ndef\nxyz", encoding: UTF-8)
         file2.txt (content: "xyz,def,abc", encoding: CP1252)
@@ -20,5 +18,18 @@ def generate_words(n=20):
     for _ in range(n):
         word = ''.join(random.choices(string.ascii_lowercase, k=random.randint(3, 10)))
         words.append(word)
+    print(words)
+    verso=(words[::-1])
+    print(verso)
 
-    return words
+    with open("file1.txt", "w", encoding="utf8") as output_file:
+        for word in words:
+                output_file.write(f"{word}\n")
+
+    #odwrócona kolejność
+    with open("file2.txt","w",encoding="cp1252") as output_2:
+        for word in verso:
+            output_2.write(f"{word} ,")
+
+
+generate_words(20)
