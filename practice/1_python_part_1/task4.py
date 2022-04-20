57a7370aa53587793b6975960e23baba8e483acb
kpodlaska <kpodlaska@griddynamics.com>
2022-04-20 10:44:18 +0200

    uzupelniony

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -4,10 +4,28 @@ subtract difference between original previous value and it's power. For first va
 Restriction:
 Examples:
     >>> calculate_power_with_difference([1, 2, 3])
-    [1, 4, 7]  # because [1^2, 2^2 - (1^2 - 1), 3^2 - (2^2 - 2)]
+    [1, 4, 7]  # because [1^2, 2^2 - (1^2 - 1), 3^2 - (2^2 - 2)] /1^2-(0^2-0)
 """
 from typing import List
 
-
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+    x=enumerate(ints)
+    y=(list(x))
+    wynik=[]
+    #print(y)
+    for i in range(len(y)):
+        if i == 0:
+            z=y[i][1]
+            #print("wartosc",i,"tego elementu wynosi",z)
+            #print("dla pierwszego elementu nic nie odejmiemy")
+            kwadrat=z**2
+            wynik.append(kwadrat)
+        else:
+          z=y[i][1]
+          #print("wartosc",i,"tego elementu wynosi",z)
+          kwadrat = z**2-((z-1)**2-(z-1))
+          wynik.append(kwadrat)
+    print(wynik)
+calculate_power_with_difference([1, 2, 3])
+calculate_power_with_difference([4,6,9])
+calculate_power_with_difference([-1,4,5,2,3,6,7])
