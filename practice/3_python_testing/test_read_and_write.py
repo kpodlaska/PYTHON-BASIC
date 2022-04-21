a39c4a2c8e635086c145b6b56a1eb0a4d31b811a
kpodlaska <kpodlaska@griddynamics.com>
2022-04-21 11:42:17 +0200

    jeszcze nie dziala - trwa przebudowa funkcji tak by testy byly latwiejsze

---
+++ practice/3_python_testing/test_read_and_write.py
@@ -0,0 +1,32 @@
+"""
+Use function 'generate_words' to generate random words.
+Write them to a new file encoded in UTF-8. Separator - '\n'.
+Write second file encoded in CP1252, reverse words order. Separator - ','.
+Example:
+    Input: ['abc', 'def', 'xyz']
+    Output:
+        file1.txt (content: "abc\ndef\nxyz", encoding: UTF-8)
+        file2.txt (content: "xyz,def,abc", encoding: CP1252)
+"""
+
+
+def generate_words():
+    import string
+    import random
+    words=list()
+    pomocnicza = list("siedem","cztery","dwa","pieski","droga","brzoza")
+    words.append(pomocnicza)
+    verso=(words[::-1])
+    print(verso)
+
+    with open("test_file_1.txt", "w", encoding="utf8") as output_file:
+        for word in words:
+                output_file.write(f"{word}\n")
+
+    #odwrócona kolejność
+    with open("test_file_2.txt","w",encoding="cp1252") as output_2:
+        for word in verso:
+            output_2.write(f"{word} ,")
+
+
+generate_words()
