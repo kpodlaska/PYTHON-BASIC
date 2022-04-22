233ff248813c314ef1659b196dfa497bd1d2ab8c
kpodlaska <kpodlaska@griddynamics.com>
2022-04-22 10:50:37 +0200

    dziala

--- practice/2_python_part_2/task_classes.py
+++ practice/2_python_part_2/task_classes.py
@@ -1,4 +1,4 @@
-""""
+"""
 Create 3 classes with interconnection between them (Student, Teacher,
 Homework)
 Use datetime module for working with date/time
@@ -22,42 +22,48 @@ Attributes:
      first_name
 Methods:
     create_homework - request task text and number of days to complete, returns Homework object
-    Note that this method doesn't need object itself
-PEP8 comply strictly.
+    Note that this method doesn't need object itselfly.
+
+PEP8 comply strict
 """
-from datetime import date, timedelta, datetime
+
+
+import datetime as dt
 
 
 class Teacher:
-    def __init__(self, last_name: str, first_name: str):
-        self.last_name = last_name
-        self.first_name = first_name
+    def __init__(self, fname, lname):
+        self.first_name = fname
+        self.last_name = lname
 
-    def create_homework(self, text, deadline, created):
-        pass
-
+    def create_homework(self, text, nod):
+        return Homework(text, nod)
 
 class Student:
-    def __init__(self, last_name: str, first_name: str):
-        self.last_name = last_name
-        self.first_name = first_name
+    def __init__(self, fname, lname):
+        self.first_name = fname
+        self.last_name = lname
 
     def do_homework(self, homework):
-        if homework.is_active == False:
-            return "Too late"
-        return "Zrobiłem zadanie"
-
+        if homework.is_active() == True:
+            return homework
+        else:
+            print('You are late')
 
 class Homework:
-    def __init__(self, text, deadline, created):
-        self.text = text
-        self.deadline = deadline
-        self.created = created
 
-    def is_active(self, homework):
-        if deadline <= 0:
+    def __init__(self, text, nod):
+        self.text = text
+        self.number_of_days = nod
+        self.created = dt.datetime.now()
+        self.deadline = self.created + \
+                        dt.timedelta(days = self.number_of_days)
+
+    def is_active(self):
+        if self.deadline >= dt.datetime.now():
+            return True
+        else:
             return False
-        return True
 
 
 if __name__ == '__main__':
@@ -66,14 +72,14 @@ if __name__ == '__main__':
     teacher.last_name  # Daniil
     student.first_name  # Petrov
 
-    expired_homework = teacher.create_homework('Learn functions', timedelta(days = 0) ,datetime(2019,3,19))
-    expired_homework.created  # Example: 2019-05-26 16:44:30.688762 #wywala błąd "noneType" object has no attribute "created"
+    expired_homework = teacher.create_homework('Learn functions', 0)
+    expired_homework.created  # Example: 2019-05-26 16:44:30.688762
     expired_homework.deadline  # 0:00:00
     expired_homework.text  # 'Learn functions'
 
     # create function from method and use it
     create_homework_too = teacher.create_homework
-    oop_homework = create_homework_too('create 2 simple classes', timedelta(days=5), datetime(2019,4,5))
+    oop_homework = create_homework_too('create 2 simple classes', 5)
     oop_homework.deadline  # 5 days, 0:00:00
 
     student.do_homework(oop_homework)
