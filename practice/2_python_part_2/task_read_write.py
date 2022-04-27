14db7565ef893a4ef772fb351c3932efb13c3d2c
kpodlaska <kpodlaska@griddynamics.com>
2022-04-27 09:59:20 +0200

    zmiana programu i aktualnie zczytuje wszystkie pliki teksowe z folderu 2_python_part_2.

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -9,15 +9,19 @@ Example:
     Output:
     result.txt(content: "23, 78, 3")
 """
-
+import os
 result=[]
-readable_file=''
-while readable_file!="koniec":
-    readable_file=input('podaj nazwe pliku w formacie text.txt, jesli chcesz zakonczyc napisz [koniec] ')
-    with open(readable_file) as content:
-        line = content.readline()
-        result.append(line)
-        print(result)
-    with open("result.txt", 'w') as output_file:
-        for line in result:
-            output_file.write(f"czesc {line}")
+listdir = '/Users/kpodlaska/PycharmProjects/pythonProject/PYTHON-BASIC/practice/2_python_part_2/'
+for f_name in os.listdir('/Users/kpodlaska/PycharmProjects/pythonProject/PYTHON-BASIC/practice/2_python_part_2/'):
+    if f_name.endswith('.txt'):
+         with open(f_name) as content:
+              line=content.readline()
+              result.append(line)
+
+         with open("result.txt","w") as output_file:
+               for line in result:
+                    output_file.write(f"{line}, ")
+"""edytowalam program tak by zapisywal zawartosc wszystkich plikow z danego folderu.
+ plik wynikowy nie jest "dopieszczony" glownie dlatego ze w folderze znajduja sie pliki wynikowe innego cwiczenia
+ i ich uklad nie jest jednorodny 
+ w wolnej chwili to przemyslenia jak zapisac to ladniej - do zabawy z result"""
