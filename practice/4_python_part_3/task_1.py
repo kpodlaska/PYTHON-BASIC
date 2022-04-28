71504da5173011283e2b50e06e5704aeaca46cbf
kpodlaska <kpodlaska@griddynamics.com>
2022-04-28 07:28:35 +0200

    jeszcze nie dziala wyjatek - brak testow

--- practice/4_python_part_3/task_1.py
+++ practice/4_python_part_3/task_1.py
@@ -10,11 +10,53 @@ If entered date is from future, return negative value for number of days
     >>> calculate_days('10-07-2021')
     WrongFormatException
 """
-from datetime import datetime
+from datetime import datetime, date
+
+"""def calculate_days(from_date: str) -> int:
+    format="%d-%m-%Y"
+    converted_date=datetime.strftime(from_date,format)
+    today = date.today()
+    delta = today - converted_date
+    return delta.days
+    print(f"Data startowa jest{converted_date}")"""
+
+# Function to convert string to datetime
+def convert(date_time):
+
+    format = '%d-%m-%Y'# The format
+    datetime_str = datetime.strptime(date_time, format)
+    start_date=date(datetime_str.year, datetime_str.month,datetime_str.day)
 
 
-def calculate_days(from_date: str) -> int:
-    ...
+    return start_date
+
+    #return datetime_str
+    #print(f"To jest data startowa to {start_date}")
+
+
+
+
+def current_date():
+    today = date.today()
+    #formatted_today=today.strftime('%d-%m-%Y')
+    return today
+    #print("Today's date:", today)
+
+
+
+def calculate_days(from_date):
+    s_date=convert(from_date) #start date
+    f_date=current_date() #current date aka finish date
+
+
+    delta = f_date - s_date
+    print(f"Pomiędzy datami było {delta.days} dni")
+
+
+convert("01-03-2020")
+current_date()
+calculate_days("22-03-2020")
+#calculate_days("2020-03-23")
 
 
 """
