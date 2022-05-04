c6b90084b81b3bf90b91f3306551248ad82c6066
kpodlaska <kpodlaska@griddynamics.com>
2022-05-04 06:03:30 +0200

    faker

--- practice/4_python_part_3/task_4.py
+++ practice/4_python_part_3/task_4.py
@@ -7,7 +7,7 @@ Exec format:
 where:
 NUMBER - positive number of generated instances
 FIELD - key used in generated dict
-PROVIDER - name of Faker provider
+PROVIDER - name of Faker provider - ma same
 Example:
 `$python task_4.py 2 --fake-address=address --some_name=name`
 {"some_name": "Chad Baird", "fake-address": "62323 Hobbs Green\nMaryshire, WY 48636"}
@@ -15,13 +15,25 @@ Example:
 """
 
 import argparse
+from faker import Faker
+fake = Faker()
+"""generate some names"""
+for _ in range(number_of_fakes):
+    print(fake.name(),fake.address())
 
-
-
-def print_name_address(args: argparse.Namespace) -> None:
+"""def print_name_address(args: argparse.Namespace) -> None:
     #creating fake address
     #random number + space + field from dict - download readable file with US zip codes or something ;)
     #then prepare recipe for building fake address
+    person=[some_name]
+    fake_address=random.randrange(1,999)+' '+random.choice(fake_street_names)+" "+random.choice(fake_cities_names)
+    print(fake_address)
+    person.append(fake_address)
+    print(person)
+
+if __name__=="__main__":
+    print(print_name_address)
+
 
 
 
